11b01ce9deca95840e72f2e82bcb375c3db4efca
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-08-18 21:53:27 +0300

    fixed path concatenation using os.path.join

--- cinema/models.py
+++ cinema/models.py
@@ -44,7 +44,7 @@ def movie_image_path(instance: "Movie", filename: str) -> pathlib.Path:
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/movies/", filename)
+    return os.path.join("uploads", "movies", filename)
 
 
 class Movie(models.Model):
